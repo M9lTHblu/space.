0fc1b5b62ab77b3de10dcba113347ee1b6f9864c
Serge <best-M9lTHblu@ya.ru>
2022-06-22 22:28:06 +0300

    Add general markup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import {Filters} from '../Filters';
+import {Footer} from '../Footer';
+import {Form} from '../Form';
 import {Header} from '../Header';
 import {Hero} from '../Hero/Hero';
+import {Posts} from '../Posts';
 
 export const App = () => {
 
@@ -9,7 +13,11 @@ export const App = () => {
       <div className="App_css" data-testid="app">
         <Header/>
         <Hero/>
-
+        <Filters>
+          <Form/>
+        </Filters>
+        <Posts/>
+        <Footer/>
       </div>
   );
 };
