85c750650539deeb7896bcfa48b8a935876feb33
Serge <best-M9lTHblu@ya.ru>
2022-06-25 17:53:29 +0300

    Edit windowResizeTo

--- __moks__/windowResizeTo.js
+++ __moks__/windowResizeTo.js
@@ -1,14 +1,14 @@
 Object.defineProperty(window, 'resizeTo', {
   writable: true,
   value: jest.fn().mockImplementation(
-      jest.fn().mockImplementation(
-          function resizeTo(width, height) {
-            Object.assign(this, {
-              innerWidth: width,
-              innerHeight: height,
-              outerWidth: width,
-              outerHeight: height
-            }).dispatchEvent(new this.Event("resize"))})
+      function resizeTo(width, height) {
+        Object.assign(this, {
+          innerWidth: width,
+          innerHeight: height,
+          outerWidth: width,
+          outerHeight: height,
+        }).dispatchEvent(new this.Event('resize'));
+      },
   ),
 });
 
